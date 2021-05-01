4eab8ffb5e08483b592ddef4b0ca1bd938a8c966
ghandalf <Ouellet.Francis@gmail.com>
2021-05-01 18:58:15 -0400

    Update libraries, version and Use TestNG

--- src/test/java/ca/ghandalf/web/analytic/TestAnalyticProviderProd.java
+++ src/test/java/ca/ghandalf/web/analytic/TestAnalyticProviderProd.java
@@ -1,33 +1,27 @@
 package ca.ghandalf.web.analytic;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import ca.ghandalf.web.analytic.AnalyticProvider;
-
-@RunWith(SpringRunner.class)
 @ActiveProfiles(TestAnalyticProviderProd.env)
 @SpringBootTest(classes = { AnalyticProvider.class })
 @ContextConfiguration
 @EnableConfigurationProperties
-public class TestAnalyticProviderProd {
+public class TestAnalyticProviderProd extends AbstractTestNGSpringContextTests {
 	
 	public static final String env = "prod";
 	
 	@Autowired
 	private AnalyticProvider provider;
 	
-	@Before
+	@BeforeClass
 	public void setUp() {
 		Assert.assertNotNull(provider);
 	}
--- src/test/java/ca/ghandalf/web/analytic/TestAnalyticProviderStage.java
+++ src/test/java/ca/ghandalf/web/analytic/TestAnalyticProviderStage.java
@@ -1,31 +1,27 @@
 package ca.ghandalf.web.analytic;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 
-import ca.ghandalf.web.analytic.AnalyticProvider;
-
-@RunWith(SpringRunner.class)
 @ActiveProfiles(TestAnalyticProviderStage.env)
 @SpringBootTest(classes = { AnalyticProvider.class })
 @ContextConfiguration
 @EnableConfigurationProperties
-public class TestAnalyticProviderStage {
+public class TestAnalyticProviderStage extends AbstractTestNGSpringContextTests {
 	
 	public static final String env = "stage";
 	
 	@Autowired
 	private AnalyticProvider provider;
 	
-	@Before
+	@BeforeClass
 	public void setUp() {
 		Assert.assertNotNull(provider);
 	}
